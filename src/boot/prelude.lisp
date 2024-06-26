dbaf518bac9c337657117bb8397dc196b5bafef0
nijakow <79372954+nijakow@users.noreply.github.com>
2024-06-26 16:55:38 +0200

    Implemented 'reduce' and a few more primitives

--- src/boot/prelude.lisp
+++ src/boot/prelude.lisp
@@ -1,9 +1,9 @@
 
-(set-symbol-function 'lmud.int:map1
+(set-symbol-function 'lmud.util:map1
    (lambda (function list)
       (if list
           (cons (funcall function (car list))
-                (lmud.int:map1 function (cdr list))))))
+                (lmud.util:map1 function (cdr list))))))
 
 (set-symbol-function 'macroexpand
    (lambda (expression)
@@ -15,7 +15,7 @@
                 (if (if (symbolp head)
                         (if (symbol-macro head) t))
                     (macroexpand (apply (symbol-macro head) args))
-                    (lmud.int:map1 #'macroexpand expression))))
+                    (lmud.util:map1 #'macroexpand expression))))
            expression)))
 
 (set-symbol-function 'compile
@@ -36,9 +36,28 @@
       (list 'set-symbol-macro (list 'quote name)
             (list 'lambda args (list* 'block name body)))))
 
-(lmud.int:map1 #'eval '(
+(lmud.util:map1 #'eval '(
    (defmacro return (&rest args)
       (list* 'return-from 'nil args))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Predicates and Primitives
+   ;;;
+
+   (defun null (e) (eq e nil))
+   (defun not  (e) (if e nil t))
+   (defun endp (e) (null e))
+   (defun atom (e) (not (consp e)))
+
+   (defun identity (e) e)
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Car, Cdr and Sequences
+   ;;;
 
    (defun caar (e) (car (car e)))
    (defun cadr (e) (car (cdr e)))
@@ -65,26 +84,38 @@
    (defun tenth   (e)  (car (cdr (cdr (cdr (cdr (cdr (cdr (cdr (cdr (cdr e)))))))))))
    (defun rest    (e)  (cdr e))
 
-   (defun null (e) (eq e nil))
-   (defun not  (e) (if e nil t))
-
-   (defun mapcar (function list &rest lists)
-      (if (not (consp list))
+   (defun last (list)
+      (if (endp (cdr list))
+          (car list)
+          (last (cdr list))))
+   
+   (defun butlast (list)
+      (if (endp (cdr list))
           nil
-          (if (null lists)
-              (cons (funcall function (car list))
-                    (mapcar  function (cdr list)))
-              (cons (apply function (car list) (mapcar #'car lists))
-                    (apply #'mapcar function (cdr list) (mapcar #'cdr lists))))))
+          (cons (car list) (butlast (cdr list)))))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Block Control Structures
+   ;;;
+
+   (defmacro prog1 (expr &rest body)
+      (let ((temp (gensym)))
+         (list 'let (list (list temp expr))
+            (cons 'progn body)
+            temp)))
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Branching Control Structures
+   ;;;
 
    (defmacro when (test &rest body)
       (list 'if test (cons 'progn body) nil))
    
    (defmacro unless (test &rest body)
       (list 'if test nil (cons 'progn body)))
-
-   (defmacro until (test &rest body)
-      (list* 'while (list 'not test) body))
 
    (defmacro cond (&rest clauses)
       (if clauses
@@ -93,7 +124,13 @@
                 (cons 'progn (cdar clauses))
                 (cons 'cond (cdr clauses)))
           nil))
-   
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Logical Control Structures
+   ;;;
+
    (defmacro and (&rest args)
       (if args
           (if (cdr args)
@@ -109,6 +146,15 @@
                     (list 'if temp temp (cons 'or (cdr args)))))
               (car args))
           nil))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Iteration and Mapping
+   ;;;
+
+   (defmacro until (test &rest body)
+      (list* 'while (list 'not test) body))
 
    (defmacro dotimes (info &rest body)
       (let ((var  (car  info))
@@ -132,12 +178,76 @@
       (let ((var  (car  info))
             (list (cadr info)))
          (list 'mapcar (list* 'lambda (list var) body) list)))
-   
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    List Operations
+   ;;;
+
    (defun member (item list)
       (cond ((null list) nil)
             ((eq item (car list)) list)
             (t (member item (cdr list)))))
    
+   (defun union (list1 list2)
+      (cond ((null list1) list2)
+            ((member (car list1) list2) (union (cdr list1) list2))
+            (t (cons (car list1) (union (cdr list1) list2)))))
+   
+   (defun intersection (list1 list2)
+      (cond ((null list1) nil)
+            ((member (car list1) list2) (cons (car list1) (intersection (cdr list1) list2)))
+            (t (intersection (cdr list1) list2))))
+   
+   (defun reduce (function sequence &key (from-end nil)
+                                         (initial-value (if from-end
+                                                            (prog1 (last sequence) (setq sequence (butlast sequence)))
+                                                            (prog1 (car sequence) (setq sequence (cdr sequence))))))
+      (if from-end
+          (if (endp sequence)
+              initial-value
+               (funcall function
+                        (car sequence)
+                        (reduce function (cdr sequence) :initial-value initial-value :from-end t)))
+          (let ((result initial-value))
+             (dolist (item sequence)
+                (setq result (funcall function result item)))
+             result)))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    List Iteration, Filtering and Mapping
+   ;;;
+
+   (defun mapcar (function list &rest lists)
+      (if (endp list)
+          nil
+          (if (null lists)
+              (cons (funcall function (car list))
+                    (mapcar  function (cdr list)))
+              (cons (apply function (car list) (mapcar #'car lists))
+                    (apply #'mapcar function (cdr list) (mapcar #'cdr lists))))))
+   
+   (defun remove-if (predicate list)
+      (cond ((endp list) nil)
+            ((funcall predicate (car list))
+             (remove-if predicate (cdr list)))
+            (t (cons (car list) (remove-if predicate (cdr list))))))
+   
+   (defun remove-if-not (predicate list)
+      (cond ((endp list) nil)
+            ((funcall predicate (car list))
+             (cons (car list) (remove-if-not predicate (cdr list))))
+            (t (remove-if-not predicate (cdr list)))))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Association Lists and Property Lists
+   ;;;
+
    (defun assoc (item alist)
       (dolist (pair alist)
          (if (eq item (car pair))
@@ -160,11 +270,23 @@
                 (setq plist (cddr plist))))
          (set-symbol-plist symbol (list* property value (symbol-plist symbol)))))
 
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Characters
+   ;;;
+
    (defun char=  (a b) (= (char-code a) (char-code b)))
    (defun char<  (a b) (< (char-code a) (char-code b)))
    (defun char>  (a b) (> (char-code a) (char-code b)))
    (defun char<= (a b) (<= (char-code a) (char-code b)))
    (defun char>= (a b) (>= (char-code a) (char-code b)))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    Non-standard conversions
+   ;;;
 
    (defun conversions:sequence->list (sequence)
       (let ((list '())
@@ -187,6 +309,12 @@
    
    (defun conversions:string->vector (string)
       (conversions:list->vector (conversions:string->list string)))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;;;
+   ;;;    REPL
+   ;;;
 
    (defun lmud.bootstrap::repl ()
       (while t
