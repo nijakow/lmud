bd238183bdebd1319808d872d97fa3c2141261b1
nijakow <79372954+nijakow@users.noreply.github.com>
2024-07-02 00:25:00 +0200

    Started to add a logger

---
+++ src/lmud/log/log.c
@@ -0,0 +1,102 @@
+
+#include <lmud/util/memory.h>
+
+#include "log.h"
+
+void LMud_Line_Create(struct LMud_Line* self, const char* text)
+{
+    self->next = NULL;
+    self->text = LMud_Strdup(text);
+}
+
+void LMud_Line_Destroy(struct LMud_Line* self)
+{
+    LMud_Free(self->text);
+}
+
+struct LMud_Line* LMud_Line_New(const char* text)
+{
+    struct LMud_Line*  line;
+    
+    line = LMud_Alloc(sizeof(struct LMud_Line));
+    
+    if (line != NULL)
+    {
+        LMud_Line_Create(line, text);
+    }
+
+    return line;
+}
+
+void LMud_Line_Delete(struct LMud_Line* self)
+{
+    LMud_Line_Destroy(self);
+    LMud_Free(self);
+}
+
+
+void LMud_Log_Create(struct LMud_Log* self)
+{
+    self->first = NULL;
+    self->last  = NULL;
+}
+
+void LMud_Log_Destroy(struct LMud_Log* self)
+{
+    struct LMud_Line*  line;
+    struct LMud_Line*  next;
+    
+    line = self->first;
+    
+    while (line != NULL)
+    {
+        next = line->next;
+        
+        LMud_Line_Destroy(line);
+        LMud_Free(line);
+        
+        line = next;
+    }
+}
+
+void LMud_Log_Append(struct LMud_Log* self, const char* text)
+{
+    struct LMud_Line*  line;
+    
+    line = LMud_Line_New(text);
+    
+    if (line != NULL)
+    {
+        if (self->first == NULL)
+        {
+            self->first = line;
+        }
+        else
+        {
+            self->last->next = line;
+        }
+        
+        self->last = line;
+    }
+}
+
+void LMud_LogComposer_Create(struct LMud_LogComposer* self, struct LMud_Log* log)
+{
+    self->log = log;
+    LMud_StringBuilder_Create(&self->builder);
+}
+
+void LMud_LogComposer_Destroy(struct LMud_LogComposer* self)
+{
+    LMud_StringBuilder_Destroy(&self->builder);
+}
+
+void LMud_LogComposer_Commit(struct LMud_LogComposer* self)
+{
+    (void) self;
+}
+
+void LMud_LogComposer_AppendChar(struct LMud_LogComposer* self, char c)
+{
+    LMud_StringBuilder_AppendChar(&self->builder, c);
+}
---
+++ src/lmud/log/log.h
@@ -0,0 +1,43 @@
+
+#pragma once
+
+#include <lmud/defs.h>
+#include <lmud/util/stringbuilder.h>
+
+struct LMud_Line
+{
+    struct LMud_Line*  next;
+    char*              text;
+};
+
+void LMud_Line_Create(struct LMud_Line* self, const char* text);
+void LMud_Line_Destroy(struct LMud_Line* self);
+
+struct LMud_Line*  LMud_Line_New(const char* text);
+void               LMud_Line_Delete(struct LMud_Line* self);
+
+
+struct LMud_Log
+{
+    struct LMud_Line*  first;
+    struct LMud_Line*  last;
+};
+
+void LMud_Log_Create(struct LMud_Log* self);
+void LMud_Log_Destroy(struct LMud_Log* self);
+
+void LMud_Log_Append(struct LMud_Log* self, const char* text);
+
+
+struct LMud_LogComposer
+{
+    struct LMud_Log*           log;
+    struct LMud_StringBuilder  builder;
+};
+
+void LMud_LogComposer_Create(struct LMud_LogComposer* self, struct LMud_Log* log);
+void LMud_LogComposer_Destroy(struct LMud_LogComposer* self);
+
+void LMud_LogComposer_Commit(struct LMud_LogComposer* self);
+
+void LMud_LogComposer_AppendChar(struct LMud_LogComposer* self, char c);
