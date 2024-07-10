c68fa5e2cc3b368136c1e96c50f88767e82cccdf
nijakow <79372954+nijakow@users.noreply.github.com>
2024-07-10 20:33:15 +0200

    Added a pattern matcher

--- src/boot/game.lisp
+++ src/boot/game.lisp
@@ -1,4 +1,64 @@
 
+(defvar game:*commands* '())
+
+
+(defmacro random:pick (&body body)
+   (let ((count (length body))
+         (index 0))
+      `(case (random ,count)
+          ,@(domap (form body)
+               (prog1 `((,index) ,form)
+                  (incf index))))))
+
+
+(defmacro game:defcommand (id pattern &body body)
+   (flet ((extract-args (pattern)
+            (remove-if-not #'symbolp pattern)))
+      `(progn
+          (push (cons (cons ',id ',pattern) (lambda () ,@body)) game:*commands*))))
+
+
+(game:defcommand :hello ("hello")
+   (io:uformat t "Hello!~%"))
+
+
+(defun game:compare-pattern-elements (input-element pattern-element)
+   (string= input-element pattern-element :key #'char-upcase))
+
+(defun game:match-basic-pattern (input pattern bindings-pass)
+   (cond ((endp pattern)
+          (if (endp input)
+              (values t bindings-pass)
+              (values nil nil)))
+         ((endp input)
+          (values nil nil))
+         ((game:compare-pattern-elements (car input) (car pattern))
+          (game:match-basic-pattern (cdr input) (cdr pattern) bindings-pass))
+         ((symbolp (car pattern))
+          (if (endp (cdr pattern))
+              (values t (cons (cons (car pattern) input) bindings-pass))
+              (multiple-value-bind (match-p bindings)
+                  (game:match-basic-pattern (cdr input) (cdr pattern) bindings-pass)
+                (if match-p
+                    (let ((binding (assoc (car pattern) bindings)))
+                       (if binding
+                           (setf (cdr binding) (cons (car input) (cdr binding)))
+                           (push (cons (car pattern) (list (car input))) bindings))
+                       (values t bindings))
+                    (values nil nil)))))
+         (t (values nil nil))))
+
+(defun game:find-command (input)
+   (dolist (command game:*commands*)
+      (multiple-value-bind (match-p bindings)
+            (game:match-basic-pattern input (cdar command) nil)
+         (if match-p
+            (return (values command bindings))))))
+
+
+(defun game:run-command (command bindings)
+   (funcall (cadr command)))
+
 (defun game:punctuationp (char)
    (member char '(#\. #\, #\! #\? #\; #\:)))
 
@@ -22,6 +82,15 @@
          (push (conversions:->string (nreverse current-word)) words))
       (nreverse words)))
 
+(defun game:huh? ()
+   (io:uformat t "~a~%"
+      (random:pick "I don't understand."
+                   "Huh?"
+                   "What?"
+                   "Pardon?"
+                   "No worky."
+                   "Say what?")))
+
 (defun game:loop (player-object)
    (while t
       (princ "> ")
@@ -33,8 +102,11 @@
                 (%signal-handler (e)
                      (lmud.bootstrap::repl (lmud.int:current-port))
                   (io:uformat t "~&An error occurred!~%")))
-               (t (princ "I don't understand that command.")
-                  (terpri))))))
+               (t (multiple-value-bind (command bindings)
+                     (game:find-command input)
+                  (if command
+                      (game:run-command command bindings)
+                      (game:huh?))))))))
 
 (defun game:banner ()
    (fresh-line)
