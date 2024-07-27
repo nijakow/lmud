c0f63e33d390106a51a1d689937c729a2c9b83f6
nijakow <79372954+nijakow@users.noreply.github.com>
2024-07-27 02:54:11 +0200

    Added color to 'tell'

--- src/mudlib/core/tell.lisp
+++ src/mudlib/core/tell.lisp
@@ -1,13 +1,40 @@
 
+(defun tell:color-code (color)
+   (case color
+      ((:black)   "30")
+      ((:red)     "31")
+      ((:green)   "32")
+      ((:yellow)  "33")
+      ((:blue)    "34")
+      ((:magenta) "35")
+      ((:cyan)    "36")
+      ((:white)   "37")
+      (t          nil)))
+
+(defun tell:compile-color (color body stream-var)
+   (let ((color-code (tell:color-code color)))
+      (unless color-code
+         (error "Invalid color code!"))
+      `(progn (princ ,(string::concatenate "\e[" color-code "m") ,stream-var)
+              (unwind-protect
+                    (progn ,@(tell:compile-forms body stream-var))
+                 (princ "\e[0m" ,stream-var))))) ;; TODO: Keep a stack of active effects
+
+(defun tell:compile-tag (tag props args stream-var)
+   (case tag
+      ((:text) `(progn ,@(tell:compile-forms args stream-var)))
+      ((:p)    `(progn ,@(tell:compile-forms args stream-var) (terpri ,stream-var)))
+      ((:color) (tell:compile-color (car props) args stream-var))
+      ((:lisp) `(princ (progn ,@args) ,stream-var))
+      (t        (error "Unknown tell tag: ~S" tag))))
+
 (defun tell:compile-form (form stream-var)
    (cond ((consp form)
-          (let ((tag  (car form))
-                (args (cdr form)))
-             (case tag
-               ((:text) `(progn ,@(tell:compile-forms args stream-var)))
-               ((:p)    `(progn ,@(tell:compile-forms args stream-var) (terpri ,stream-var)))
-               ((:lisp) `(princ (progn ,@args) ,stream-var))
-               (t        (error "Unknown tell tag: ~S" tag)))))
+          (multiple-value-bind (tag props args)
+               (if (consp (car form))
+                   (values (caar form) (cdar form) (cdr form))
+                   (values (car form) nil (cdr form)))
+            (tell:compile-tag tag props args stream-var)))
          (t `(princ ,form ,stream-var))))
 
 (defun tell:compile-forms (forms stream-var)
