d6185aed782759de94e0ddfa3e0b5a6ac3b01605
nijakow <79372954+nijakow@users.noreply.github.com>
2024-11-23 17:16:53 +0100

    Added telnet

--- src/lisp/kernel/engine/io/telnet.lisp
+++ src/lisp/kernel/engine/io/telnet.lisp
@@ -10,8 +10,53 @@
 (tos:defmethod (telnet:<telnet-port> telnet::basic-write-byte) (byte)
    (tos:xsend io:<wrapped-port-stream> self 'write-byte byte))
 
+(tos:defmethod (telnet:<telnet-port> telnet::begin-receive-sub-negotiation) ()
+   ;; Only read in the data - don't do anything with it
+   (let ((command (.telnet::basic-read-byte self))
+         (option  (.telnet::basic-read-byte self)))
+      nil))
+
+(tos:defmethod (telnet:<telnet-port> telnet::begin-receive-will) ()
+   (let ((option (.telnet::basic-read-byte self)))
+      (.telnet::basic-write-byte self 255)
+      (.telnet::basic-write-byte self 252)
+      (.telnet::basic-write-byte self option)))
+
+(tos:defmethod (telnet:<telnet-port> telnet::begin-receive-wont) ()
+   (let ((option (.telnet::basic-read-byte self)))
+      (.telnet::basic-write-byte self 255)
+      (.telnet::basic-write-byte self 252)
+      (.telnet::basic-write-byte self option)))
+
+(tos:defmethod (telnet:<telnet-port> telnet::begin-receive-do) ()
+   (let ((option (.telnet::basic-read-byte self)))
+      (.telnet::basic-write-byte self 255)
+      (.telnet::basic-write-byte self 252)
+      (.telnet::basic-write-byte self option)))
+
+(tos:defmethod (telnet:<telnet-port> telnet::begin-receive-dont) ()
+   (let ((option (.telnet::basic-read-byte self)))
+      (.telnet::basic-write-byte self 255)
+      (.telnet::basic-write-byte self 252)
+      (.telnet::basic-write-byte self option)))
+
+(tos:defmethod (telnet:<telnet-port> telnet::begin-receive-command) ()
+   (let ((command (.telnet::basic-read-byte self)))
+      (case command
+         ((250) (.telnet::begin-receive-sub-negotiation self))
+         ((251) (.telnet::begin-receive-will            self))
+         ((252) (.telnet::begin-receive-wont            self))
+         ((253) (.telnet::begin-receive-do              self))
+         ((254) (.telnet::begin-receive-dont            self))
+         ((255) nil)
+         (t (error "Unknown telnet command: ~S" command)))))
+
 (tos:defmethod (telnet:<telnet-port> read-byte) ()
-   (.telnet::basic-read-byte self))
+   (let ((byte (.telnet::basic-read-byte self)))
+      (if (= byte 255)
+          (progn (.telnet::begin-receive-command self)
+                 (.read-byte self)) ; This is recursive -- turn this into a loop?
+          byte)))
 
 (tos:defmethod (telnet:<telnet-port> write-byte) (byte)
    (if (= byte 255)
