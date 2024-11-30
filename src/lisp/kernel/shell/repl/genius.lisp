03f79fefa15d1afc61d44b61afb7e49c9bcc2009
nijakow <79372954+nijakow@users.noreply.github.com>
2024-11-30 15:49:19 +0100

    Minor changes

--- src/lisp/kernel/shell/repl/genius.lisp
+++ src/lisp/kernel/shell/repl/genius.lisp
@@ -1,5 +1,6 @@
 
-(tos:defclass genius::<genius-reporter> (io.reader:<reporter>))
+(tos:defclass genius::<genius-reporter> (io.reader:<reporter>)
+   (with (tokens nil)))
 
 (tos:defmethod (genius::<genius-reporter> report-data) (data &key from to)
    (lmud.dummy:%princ "[GENIUS] From: ")
@@ -10,10 +11,17 @@
    (lmud.dummy:%prin1 data)
    (lmud.dummy:%princ " / ")
    (lmud.dummy:%prin1 (car .openings))
-   (lmud.dummy:%terpri))
+   (lmud.dummy:%terpri)
+   (push (cons (cons from to) data) .tokens))
+
+(tos:defmethod (genius::<genius-reporter> get-tokens) ()
+   .tokens)
 
 (defun genius::make-reporter ()
    (tos:make-instance genius::<genius-reporter>))
+
+(defun genius:generate-styles (tokens)
+   nil)
 
 (defun genius:analyze-string (text)
    (let ((stream   (io:make-string-stream text))
@@ -24,7 +32,10 @@
                                   :eof-value   eof-value)))
          (if (eq result eof-value)
              (progn (lmud.dummy:%princ "EOF")
-                    (lmud.dummy:%terpri))))))
+                    (lmud.dummy:%terpri)))
+         (list (cons :success (not (eq result eof-value)))
+               (cons :result  result)
+               (cons :styles  (genius:generate-styles (.get-tokens reporter)))))))
 
 (defun genius:read-line ()
    (readline:read-line :prompt           "@ "
